e0c61ee77bf045d2ac249f6fae7c85ab768561f6
Nathan Andrew <nathannxx@proton.me>
2025-01-20 22:46:42 +0000

    RMX1941: vendorsetup: add FMRadio repository
    Signed-off-by: Nathan Andrew <nathannxx@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,6 @@
 echo "Cloning Vendor Tree"
 git clone https://github.com/nathanzerogarage/vendor_realme_RMX1941 -b eleven vendor/realme/RMX1941
+
+echo "Cloning FMRadio Repository"
+rm -rf packages/apps/FMRadio
+git clone https://github.com/techyminati/android_packages_apps_FMRadio -b lineage-18.1-C2 packages/apps/FMRadio
