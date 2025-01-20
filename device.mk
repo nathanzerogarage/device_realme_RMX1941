42239539cfafef7c8ed38b90dd0c67def9957d9a
imnathanzero <nathannxx@proton.me>
2025-01-21 01:19:20 +0700

    oops, missing `xd-overlay`
    Signed-off-by: imnathanzero <nathannxx@proton.me>

--- device.mk
+++ device.mk
@@ -28,7 +28,8 @@ PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-lineage \
+    $(DEVICE_PATH)/overlay-xd
 
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 29
