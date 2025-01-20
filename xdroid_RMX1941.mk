fd46ec2b6cf19c8ea2dc68dc37027c80da9b9097
Nathan Andrew <nathannxx@proton.me>
2025-01-21 02:23:57 +0700

    RMX1941: `xd-ify` (1)

--- xdroid_RMX1941.mk
+++ xdroid_RMX1941.mk
@@ -38,7 +38,7 @@ TARGET_SCREEN_WIDTH := 720
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX1941
-PRODUCT_NAME := xd_RMX1941
+PRODUCT_NAME := xdroid_RMX1941
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme C2
 PRODUCT_MANUFACTURER := realme
